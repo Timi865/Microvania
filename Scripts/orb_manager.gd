89e2ca3afefe96763f8619ff4887560c2659b1ed
Timi865 <252792114+Timi865@users.noreply.github.com>
2026-09-22 20:50:39 +0100

    orbmanager and orbs
    Fixed the bug that came with making orb1 and orb2. added orb3

--- Scripts/orb_manager.gd
+++ Scripts/orb_manager.gd
@@ -1,13 +1,12 @@
 extends Node2D
 
-@onready var orb: Node2D = $Orb
+@onready var orbs: Array[Node2D] = [$Orb1, $Orb2]
 
 var target_position := Vector2(2, -14)
 
 func _ready() -> void:
-	orb.position = target_position
+	for orb in orbs:
+		orb.position = target_position
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
